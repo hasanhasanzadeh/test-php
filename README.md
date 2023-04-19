633104a2543b513b9a7d846f6faecc72a42df981
hassan hassanzadeh <5064924-hassan.hassanzadeh@users.noreply.gitlab.com>
2023-04-19 13:07:58 +0430

    first test commit

---
+++ README.md
@@ -0,0 +1,5 @@
+<h2>This is For Test PHP</h2>
+<br>
+<a href="https://zhihatgroup.ir" target="_blank">
+Zhihat Group
+</a>
---
+++ debug.php
@@ -0,0 +1,5 @@
+<?php
+function dd($variable)
+{
+    die("<div style='background-color:#181717;color:#b6ab14;font-size:17px;padding:10px;border-radius: 5px;'> var_dump($variable)</div>");
+}
